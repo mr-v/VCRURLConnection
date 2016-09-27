3471ca25c5c2eacd2bf1f5c6c68f955d881e7e03
Witold Skibniewski <witold.skibniewski@gmail.com>
2016-09-27 11:21:57 +0200

    Add import of public header to umbrella header

--- VCRURLConnection/VCRURLConnection.h
+++ VCRURLConnection/VCRURLConnection.h
@@ -15,3 +15,5 @@ FOUNDATION_EXPORT double VCRURLConnectionVersionNumber;
 FOUNDATION_EXPORT const unsigned char VCRURLConnectionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VCRURLConnection/PublicHeader.h>
+
+#import <VCRURLConnection/VCR.h>
